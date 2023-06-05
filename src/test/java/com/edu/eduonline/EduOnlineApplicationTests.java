28d7a6de0836726f74bba96bf73ad2408f867fe7
hzwDec <2944535274@qq.com>
2023-06-05 14:47:24 +0800

    Initial commit

---
+++ src/test/java/com/edu/eduonline/EduOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.eduonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
